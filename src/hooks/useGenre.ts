6246b659723f4f621bb448d08af06e05b45e4563
im-vishu <vishant.22btcse0138@dbuumail.in>
2026-05-07 01:36:11 +0530

    fix(ui): increase z-index for filter dropdowns

--- src/hooks/useGenre.ts
+++
@@ -1,8 +0,0 @@
-import useGenres from "./useGenres";
-
-const useGenre = (id?: number) => {
-  const { data: genre } = useGenres();
-  return genre?.results.find((g) => g.id === id);
-};
-
-export default useGenre;
